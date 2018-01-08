8c2aedacbe4be9612845ae80149cbd481ede7dbf
hoainampro <namhoai.071096@gmail.com>
2018-01-08 08:52:15 +0700

    masterHTML

---
+++ README.md
@@ -0,0 +1,30 @@
+# masterHtml
+a Gulp project for HTML working by PhuPT
+
+**Supported:**
+- _js:_ concat, hint, minify, convert es6 to es5
+- _css:_ sass, concat, minify, auto prefixer to work in all browsers
+- _html:_ include file, pass parameters... like php project
+- _browser:_ auto sync when files changed, no need to reload manually
+- _bower:_ to manage your packages
+
+**PreInstall Packages:**
+- _lodash_
+- _fontawesome_
+- _flexslider_
+- _bootstrap_
+- _jquery_
+- _fancybox_
+
+**Structure:**
+- _source:_ workspace - place to work
+- _dist:_ distribution - place to run
+
+**Using:**
+- Clone this
+- run _npm install_ to download all node modules
+- open command line then go to masterHtml by using _cd_ command
+- run _gulp_ command to start
+- That's!
+
+Contact me: **phuptqn@gmail.com**
---
+++ source/js/main.js
@@ -0,0 +1,50 @@
+(function($) {
+
+	$(function() {
+       
+	});
+
+	$(window).load(function() {
+
+	});
+
+
+	$(document).ready(function() {
+
+		var timeoutId;
+
+		$('#ul_slider li img').click(function(){
+			var imgbg = $(this).attr('src');
+			//console.log(imgbg);
+			$('#bg').css({backgroundImage: "url("+imgbg+")"});
+		});
+		
+		$('a.fa-chevron').click(function() {
+        	$('.content_about').toggle();
+        	return false;
+    	});
+
+		//When clicked on Pause
+		$('#pause').click(function() {
+
+		   //Clear timeout
+		   clearTimeout ( timeoutId );    
+		    
+		    //Hide Pause and show Play
+		    $(this).hide();
+		    $('#play').show();
+		});
+
+		//When clicked on Play
+		$('#play').click(function() {
+		   
+		   //Start slide image
+		   //slideImage(0);
+		   //Hide Play and show Pause
+		   $(this).hide();
+		   $('#pause').show();    
+		});
+
+	});
+
+})(jQuery);
